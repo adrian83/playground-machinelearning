40c83f533935525214ebca659ce96d73c2656f3f
adrian83 <adrianbrzoza1983@gmail.com>
2021-09-10 20:15:11 +0200

    cleaning

--- tensorflow/fashion-mnist/run.py
+++ tensorflow/fashion-mnist/run.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
+
+EPOCHS = 10
 
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
@@ -10,27 +10,12 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-
-
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
-
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(len(class_names))
 ])
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -39,13 +24,15 @@ model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=EPOCHS)
 
 #test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 model.evaluate(test_images,  test_labels, verbose=2)
 
-probability_model = tf.keras.Sequential([model, 
-                                         tf.keras.layers.Softmax()])
+probability_model = tf.keras.Sequential([
+    model, 
+    tf.keras.layers.Softmax()
+])
 
 
 predictions = probability_model.predict(test_images)
--- tensorflow/mnist/run.py
+++ tensorflow/mnist/run.py
@@ -1,34 +1,31 @@
 import tensorflow as tf
 
+CLASSES_CONT = 10 # there is 10 digits
+EPOCHS = 10
+
+
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
 
-#print(str(x_train))
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+  tf.keras.layers.Dense(CLASSES_CONT)
 ])
 
-#print(str(model))
-
-#predictions = model(x_train[:1]).numpy()
-
-#tf.nn.softmax(predictions).numpy()
-
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-#loss_fn(y_train[:1], predictions).numpy()
 
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=8)
+model.fit(x_train, y_train, epochs=EPOCHS)
 
 model.evaluate(x_test,  y_test, verbose=2)
 
@@ -37,7 +34,9 @@ probability_model = tf.keras.Sequential([
   tf.keras.layers.Softmax()
 ])
 
-print(str(probability_model(x_test[:5])))
+predictions = probability_model.predict(x_test)
+
+print(str(predictions[:5]))
 
 
 
